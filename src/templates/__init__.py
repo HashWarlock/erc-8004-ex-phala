4ce67a11d2fd260183cdb73cab7c573bbfcd89b5
HashWarlock <hashwarlock@protonmail.com>
2025-10-03 13:23:25 +0800

    fix: Remove deleted templates from imports

--- src/templates/__init__.py
+++ src/templates/__init__.py
@@ -1,17 +1,5 @@
-"""
-Agent Templates
-
-Pre-built agent templates for common use cases.
-"""
+"""Agent Templates"""
 
 from .server_agent import ServerAgent
-from .validator_agent import ValidatorAgent
-from .client_agent import ClientAgent
-from .custom_agent import CustomAgent
 
-__all__ = [
-    'ServerAgent',
-    'ValidatorAgent',
-    'ClientAgent',
-    'CustomAgent'
-]
+__all__ = ['ServerAgent']
